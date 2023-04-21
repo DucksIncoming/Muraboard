57c34e8db48eca9769e79c996515e0963a126baa
DucksIncoming <ducksincoming@gmail.com>
2023-04-21 16:07:48 -0400

    edit + mark as complete done

--- js/dashboard.js
+++ js/dashboard.js
@@ -36,8 +36,12 @@ function packageReminder() {
     var reminderDate = document.getElementById("new-reminder-datetime");
     var reminderResource = document.getElementById("new-reminder-links");
 
-    reminderTagsElms = document.getElementById("new-reminder-selected-tags");
-    reminderTagsElms = reminderTagsElms.children;
+    if (reminderTitle.value == ""){
+        return;
+    }
+
+    reminderTagsParent = document.getElementById("new-reminder-selected-tags");
+    reminderTagsElms = reminderTagsParent.children;
     let rTags = []
 
     for (let i=0; i<reminderTagsElms.length; i++){
@@ -45,28 +49,42 @@ function packageReminder() {
     }
 
     storeReminder(reminderTitle.value, reminderDesc.value, reminderDate.valueAsDate, reminderResource.value, rTags);
-    closeNewReminder();
+    reminderTitle.value = "";
+    reminderDesc.value = "";
+    reminderDate.valueAsDate = null;
+    reminderResource.value = "";
 }
 
 function storeReminder(title, desc, date, resources, tags){
-    let reminderHTML = '<div class="reminder-card" id=[TITLE]> <p class="reminder-card-title">[TITLE]</p> <p class="reminder-card-description">[DESCRIPTION]</p> <p class="reminder-card-resource">[RESOURCES]</p> <p class="reminder-card-date">[DATE]</p> <p class="reminder-card-timeleft">[REMAINING]</p> <input type="image" src="Assets/Images/edit_icon.png" class="reminder-card-edit" alt="" /> <input type="image" src="Assets/Images/checkbox_icon.png" class="reminder-card-check" alt="" /> <input type="image" src="Assets/Images/trash_icon.png" class="reminder-card-trash" alt="" /> </div>'
-    reminderHTML = reminderHTML.replaceAll("[TITLE]", title);
+    let reminderHTML = '<div class="reminder-card" id=[TITLE]> <p class="reminder-card-title">[TITLE]</p> <p class="reminder-card-description">[DESCRIPTION]</p> <p class="reminder-card-resource">[RESOURCES]</p> <p class="reminder-card-date">[DATE]</p> <p class="reminder-card-timeleft">[REMAINING]</p> <input type="image" src="Assets/Images/edit_icon.png" class="reminder-card-edit" alt="" /> <input type="image" src="Assets/Images/checkbox_icon.png" onclick="markReminder(this.parentNode)" class="reminder-card-check" alt="" /> <input type="image" src="Assets/Images/trash_icon.png" class="reminder-card-trash" onclick="return this.parentNode.remove();" alt="" /> </div>'
+    index = document.getElementsByClassName("reminder-card").length;
+    reminderHTML = reminderHTML.replace("[TITLE]", title + index.toString());
+    reminderHTML = reminderHTML.replace("[TITLE]", title);
     reminderHTML = reminderHTML.replace("[DESCRIPTION]", desc);
-    reminderHTML = reminderHTML.replace("[DATE]", formatDate(date));
 
-    let today = new Date();
-
-    reminderHTML = reminderHTML.replace("[REMAINING]", daysRemaining(today, date).toString() + " days")
+    if (date != null) {
+        reminderHTML = reminderHTML.replace("[DATE]", formatDate(date));
+        let today = new Date();
+        reminderHTML = reminderHTML.replace("[REMAINING]", daysRemaining(today, date).toString() + " days")
+    }
+    else {
+        reminderHTML = reminderHTML.replace("[DATE]", "Undated");
+        reminderHTML = reminderHTML.replace("[REMAINING]", "");
+    }
+    
     reminderHTML = reminderHTML.replace("[RESOURCES]", resources);
     insertHTML(reminderHTML, "reminders");
 
+
     let tagHTML = '<div class="reminder-card-tag" style="background-color:[COLOR]">[TAGNAME]</div>'
-    
+
     for (let i=0; i<tags.length; i++){
         thisTag = tagHTML.replace("[TAGNAME]", tags[i]);
         thisTag = thisTag.replace("[COLOR]", "yellowgreen");
-        addFirstHTML(thisTag, title);
+        addFirstHTML(thisTag, title + index.toString());
     }
+
+    closeNewReminder();
 }
 
 function selectNewReminderTag(tagName) {
@@ -81,11 +99,26 @@ function selectNewReminderTag(tagName) {
 function closeNewReminder() {
     reminders_newReminder.style.visibility = "hidden";
     screenDarken.style.visibility = "hidden";
+    reminders_newReminder_tagSelector.visibility = "hidden";
+}
+
+function editReminder(reminder) {
+    createNewReminder();
+    
+    var reminderTitle = document.getElementById("new-reminder-name");
+    var reminderDesc = document.getElementById("new-reminder-description");
+    var reminderDate = document.getElementById("new-reminder-datetime");
+    var reminderResource = document.getElementById("new-reminder-links");
+
+    reminderTitle.value = reminder.querySelector("#reminder-card-title").value;
+    reminderDesc.value = reminder.querySelector("#reminder-card-description").value;
+    reminderResource.value = reminder.querySelector("#reminder-card-resource").value;
 }
 
 function createNewReminder() {
     reminders_newReminder.style.visibility = "visible";
     screenDarken.style.visibility = "visible";
+    document.getElementById("new-reminder-selected-tags").innerHTML = "";
 }
 
 function formatDate(date) {
@@ -104,4 +137,17 @@ function daysRemaining(date1, date2) {
 
 function getTagColor() {
     return 'red';
+}
+
+function markReminder(reminder){
+    if (reminder.style.textDecoration == "line-through"){
+        reminder.style.textDecoration = "none"
+        reminder.style.opacity = 1.0;
+        reminder.style.backgroundColor = "#ffffff";
+    }
+    else {
+        reminder.style.textDecoration = "line-through"
+        reminder.style.opacity = 0.6;
+        reminder.style.backgroundColor = "#B1FFD8";
+    }
 }
